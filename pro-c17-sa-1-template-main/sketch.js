56f604f80464a669e950ab9ecded0c270e959996
CKSoratto <97638142+CKSoratto@users.noreply.github.com>
2022-03-02 18:36:35 -0300

    Add files via upload

---
+++ pro-c17-sa-1-template-main/sketch.js
@@ -0,0 +1,33 @@
+var aa
+
+
+
+
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+
+aa = new Box(140, 150, 14, 13, 15, 8);
+
+
+
+
+}
+
+function draw() 
+{
+  background(220);
+
+aa.show();
+aa.move();
+
+
+
+
+}
+
+
+
+
